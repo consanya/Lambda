99f92274664a9b75be55e5372ea30d8f6fee3aa9
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-09-21 13:27:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-exports.handler = async (event) => {
+iexports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda and Github test!"),
+    body: JSON.stringify("Hello from Lambda and Github test! github actions"),
   }
   return response
 }
